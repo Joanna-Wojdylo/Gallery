fe6fda422db41af79a94c8b5a44c85d81530ba83
Joanna-Wojdylo <a.wojdylo29@gmail.com>
2020-03-30 17:43:49 +0200

    Initial commit - photos are taken from hardcoded list, after click picture is shown on fullscreen

---
+++ app/src/main/java/com/example/gallery_test/ImageAdapter.java
@@ -0,0 +1,53 @@
+package com.example.gallery_test;
+
+import android.content.Context;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.GridView;
+import android.widget.ImageView;
+
+public class ImageAdapter extends BaseAdapter {
+
+    private Context mContext;
+    public int[] imageArray = {
+            R.drawable.bal, R.drawable.myszojelen, R.drawable.plakat,
+            R.drawable.pic1, R.drawable.pic2, R.drawable.pic3,
+            R.drawable.pic4, R.drawable.pic5, R.drawable.pic6,
+            R.drawable.pic7, R.drawable.pic8, R.drawable.pic9,
+            R.drawable.pic10, R.drawable.pic10, R.drawable.pic12,
+            R.drawable.bal, R.drawable.myszojelen, R.drawable.plakat,
+            R.drawable.pic1, R.drawable.pic2, R.drawable.pic3,
+            R.drawable.pic4, R.drawable.pic5, R.drawable.pic6,
+            R.drawable.pic7, R.drawable.pic8, R.drawable.pic9,
+            R.drawable.pic10, R.drawable.pic10, R.drawable.pic12};
+
+    public ImageAdapter(Context mContext) {
+        this.mContext = mContext;
+    }
+
+    @Override
+    public int getCount() {
+        return imageArray.length;
+    }
+
+    @Override
+    public Object getItem(int position) {
+        return imageArray[position];
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return 0;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        ImageView imageView = new ImageView(mContext);
+        imageView.setImageResource(imageArray[position]);
+        imageView.setScaleType(ImageView.ScaleType.CENTER_CROP);
+        imageView.setLayoutParams(new GridView.LayoutParams(340,350));
+
+        return imageView;
+    }
+}
---
+++ app/src/main/java/com/example/gallery_test/MainActivity.java
@@ -0,0 +1,40 @@
+package com.example.gallery_test;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemClickListener;
+import android.widget.GridView;
+
+public class MainActivity extends Activity {
+
+    GridView gridView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        GridView gridView = (GridView) findViewById(R.id.gridView);
+
+        //Instance of ImageAdapter Class
+        gridView.setAdapter(new ImageAdapter(this));
+
+        //On Click event for Single GridView Item
+        gridView.setOnItemClickListener(new AdapterView.OnItemClickListener(){
+            @Override
+            public void onItemClick (AdapterView<?> parent, View v, int position, long id){
+                //Send image id to FullImageActivity
+                Intent i = new Intent(getApplicationContext(), FullImageActivity.class);
+                //passing array index
+                i.putExtra("id", position);
+                startActivity(i);
+            }
+        });
+    }
+
+}
