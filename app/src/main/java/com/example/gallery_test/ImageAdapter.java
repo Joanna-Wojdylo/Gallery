7eea902caa20d075faf9bf4c4e98139b6e10c26a
Joanna-Wojdylo <a.wojdylo29@gmail.com>
2020-04-02 17:16:23 +0200

    Photos are loaded from device memory (internal, not sure what folder and why). Removed fullscreen option.

--- app/src/main/java/com/example/gallery_test/ImageAdapter.java
+++ app/src/main/java/com/example/gallery_test/ImageAdapter.java
@@ -1,16 +1,24 @@
 package com.example.gallery_test;
 
 import android.content.Context;
+import android.media.Image;
+import android.net.Uri;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
 import android.widget.GridView;
 import android.widget.ImageView;
+import android.database.Cursor;
+import android.provider.MediaStore;
+
 
 public class ImageAdapter extends BaseAdapter {
 
     private Context mContext;
-    public int[] imageArray = {
+    private Cursor cursor;
+    int columnIndex;
+
+    /**public int[] imageArray = {
             R.drawable.bal, R.drawable.myszojelen, R.drawable.plakat,
             R.drawable.pic1, R.drawable.pic2, R.drawable.pic3,
             R.drawable.pic4, R.drawable.pic5, R.drawable.pic6,
@@ -21,19 +29,21 @@ public class ImageAdapter extends BaseAdapter {
             R.drawable.pic4, R.drawable.pic5, R.drawable.pic6,
             R.drawable.pic7, R.drawable.pic8, R.drawable.pic9,
             R.drawable.pic10, R.drawable.pic10, R.drawable.pic12};
-
-    public ImageAdapter(Context mContext) {
+*/
+    public ImageAdapter(Context mContext, Cursor cursor, int columnIndex) {
         this.mContext = mContext;
+        this.cursor = cursor;
+        this.columnIndex = columnIndex;
     }
 
     @Override
     public int getCount() {
-        return imageArray.length;
+        return cursor.getCount(); //<------ TU POTRZEBUJE KURSORA Z MAIN -------------------------
     }
 
     @Override
     public Object getItem(int position) {
-        return imageArray[position];
+        return position;
     }
 
     @Override
@@ -43,11 +53,33 @@ public class ImageAdapter extends BaseAdapter {
 
     @Override
     public View getView(int position, View convertView, ViewGroup parent) {
+        ImageView imageView;
+        if (convertView == null) {
+            imageView = new ImageView(mContext);
+            //Move cursor to current position
+            cursor.moveToPosition(position);
+            //Get the current value for the requested column
+            int imageID = cursor.getInt(columnIndex);
+            //Set the content of the image based on the provider URI
+            imageView.setImageURI(Uri.withAppendedPath(
+                    MediaStore.Images.Thumbnails.EXTERNAL_CONTENT_URI, "" + imageID));
+            imageView.setScaleType(ImageView.ScaleType.CENTER_CROP);
+            //imageView.setPadding(5, 5, 5, 5);
+            imageView.setLayoutParams(new GridView.LayoutParams(350, 350));
+        }
+        else {
+            imageView = (ImageView) convertView;
+        }
+        return imageView;
+        }
+
+        /**
         ImageView imageView = new ImageView(mContext);
         imageView.setImageResource(imageArray[position]);
         imageView.setScaleType(ImageView.ScaleType.CENTER_CROP);
         imageView.setLayoutParams(new GridView.LayoutParams(340,350));
 
         return imageView;
+         */
     }
-}
+
--- app/src/main/java/com/example/gallery_test/MainActivity.java
+++ app/src/main/java/com/example/gallery_test/MainActivity.java
@@ -1,17 +1,28 @@
 package com.example.gallery_test;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.app.Activity;
+import android.content.Context;
 import android.content.Intent;
+import android.database.Cursor;
+import android.os.Build;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.view.View;
 import android.widget.AdapterView;
-import android.widget.AdapterView.OnItemClickListener;
 import android.widget.GridView;
+import android.provider.MediaStore;
+import android.net.Uri;
+import android.widget.AdapterView.OnItemClickListener;
+
+
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends Activity {
-
+    public Cursor cursor; //cursor to access the results from querying for images on the SD card
+    private int columnIndex; //column index for the Thumbnails Image IDs
     GridView gridView;
 
     @Override
@@ -19,13 +30,42 @@ public class MainActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        GridView gridView = (GridView) findViewById(R.id.gridView);
+        //Set up an array of the Thumbnail Image ID column we want
+        String[] projection = {MediaStore.Images.Thumbnails._ID};
+        //Create cursor pointing to the SDCard
+        cursor = getContentResolver().query(MediaStore.Images.Thumbnails.EXTERNAL_CONTENT_URI,
+                projection, // which columns to return
+                null, //Return all rows
+                null,
+                MediaStore.Images.Thumbnails._ID+""); // Sorted by image ID (probably)
+        //Get the column index of the Thumbnails Image ID
+        assert cursor != null;
+        columnIndex = cursor.getColumnIndexOrThrow(MediaStore.Images.Thumbnails._ID);
+
+        GridView gridView;
+        gridView = (GridView) findViewById(R.id.gridView);
 
         //Instance of ImageAdapter Class
-        gridView.setAdapter(new ImageAdapter(this));
+        gridView.setAdapter(new ImageAdapter(this, cursor, columnIndex));
 
         //On Click event for Single GridView Item
-        gridView.setOnItemClickListener(new AdapterView.OnItemClickListener(){
+        gridView.setOnItemClickListener(new OnItemClickListener() {
+            public void onItemClick(AdapterView parent, View v, int position, long id){
+                //Get the data location of the image
+                String [] projection = {MediaStore.Images.Media.DATA};
+                cursor = getContentResolver().query(MediaStore.Images.Media.EXTERNAL_CONTENT_URI,
+                        projection, // which columns to return
+                        null, //Return all rows
+                        null,
+                        null);
+                assert cursor != null;
+                columnIndex = cursor.getColumnIndexOrThrow(MediaStore.Images.Media.DATA);
+                cursor.moveToPosition(position); //get image filename
+                String imagePath = cursor.getString(columnIndex); //this path will be used to do further processing ex. full screen display
+            }
+
+        });
+        /**gridView.setOnItemClickListener(new AdapterView.OnItemClickListener(){
             @Override
             public void onItemClick (AdapterView<?> parent, View v, int position, long id){
                 //Send image id to FullImageActivity
@@ -35,6 +75,7 @@ public class MainActivity extends Activity {
                 startActivity(i);
             }
         });
+         */
     }
 
 }
